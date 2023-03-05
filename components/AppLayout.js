91ce1840ffd5f0273f27447723d1c09f75f89295
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-03-04 22:21:37 -0300

    preparing to setup sass

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from "@/styles/AppLayout.module.css"
 
 export default function AppLayout({children}){
     return(
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      
-      <main></main>
+    
+    <>      
 
     </>
+
   )
 }
