99e7d86e6006797fa4093dd7e8890b73fde29c2c
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-03-05 06:12:56 -0300

    first demo

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,11 +14,11 @@ export default function Home() {
         }}>
           <ProfilePicture></ProfilePicture>
           <div>
-            <h1>vinicius</h1>
-            <h1>el</h1>
-            <h1>khalili</h1>
+            <h1>Such</h1>
+            <h1>a nice</h1>
+            <h1>component</h1>
             <hr style={{width:"40%",margin:"20px auto 20px auto"}}/>
-            <h3>creative developer</h3>
+            <h3>neat</h3>
           </div>
         </div>
       </div>
